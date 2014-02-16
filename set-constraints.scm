1aa47cd89947918b90b0462f558ff9caa864a34b
William E. Byrd <webyrd@gmail.com>
2014-02-15 22:22:31 -0700

    implementing algorithm A

---
+++ set-constraints.scm
@@ -0,0 +1,8 @@
+
+;; Algorithm A from p.4 of Stolzenburg's 'Membership-Constraints and
+;; Complexity in Logic Programming with Sets'
+;;
+;; Algorithm A simplifies a membership constraint of the form
+;; C = memb(x, L), where L = [y1,...,yn].
+(define algorithm-A
+  )
